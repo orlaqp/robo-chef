e6488211d0809913045b4512b337fbd0e825df0f
Orlando Quero <orlaqp@bincrafters.com>
2022-09-12 08:07:22 -0400

    fixed config issue

--- libs/web/graphql/src/index.ts
+++ libs/web/graphql/src/index.ts
@@ -3,7 +3,6 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 console.log(`Setting apolloclient to: ${process.env['NEXT_PUBLIC_GRAPHQL_ENDPOINT']}`);
 
 export const apolloClient = new ApolloClient({
-    // uri: process.env['NEXT_PUBLIC_GRAPHQL_ENDPOINT'],
-    uri: 'http://localhost:3333/graphql',
+    uri: process.env['NEXT_PUBLIC_GRAPHQL_ENDPOINT'],
     cache: new InMemoryCache(),
 });
